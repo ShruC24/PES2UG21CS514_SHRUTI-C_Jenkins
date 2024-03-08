9917c2729bad12f2576222c7fd439a2b4d6d1209
SHRUTI C <107233243+ShruC24@users.noreply.github.com>
2024-03-08 11:08:27 +0530

    HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,1 +1,9 @@
-#include<iostream>
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "SHRUTI C - PES2UG21CS514\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
